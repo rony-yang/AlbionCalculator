62665f6783ccfa4775338c904edc25f2629226d1
Seyoung <seyoung5157@gmail.com>
2024-06-03 16:29:25 +0900

    3티어까지 완성

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+  res.render('mainPage');
 });
