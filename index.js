ff5d2a65e21588d08ebdbe81c36c0b9a8c822025
Seyoung <seyoung5157@gmail.com>
2024-06-12 13:51:53 +0900

    요리 완성

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.listen(port, () => {
 app.set('view engine', 'ejs');
 app.set('views', './views')
 
-app.get('/', (req, res) => {
+app.get('/albion', (req, res) => {
   res.render('mainPage');
+});
+
+app.get('/chickenOmelette', (req, res) => {
+  res.render('chickenOmelette');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('오류');
 });
